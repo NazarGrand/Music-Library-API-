24bb56442344f1b07d5220d6759b5249d0a7fca7
NazarGrand <hryhoriak.nazarii@chnu.edu.ua>
2024-03-11 16:40:07 +0200

    fix: removed loader from a home page

--- Frontend/src/pages/Home/HomePage.jsx
+++ Frontend/src/pages/Home/HomePage.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import Loader from "../../components/Loader/Loader";
 
 const HomePage = () => {
-  return (
-    // <h1>Home Page</h1>
-    <Loader />
-  );
+  return <h1>Home Page</h1>;
 };
 
 export default HomePage;
